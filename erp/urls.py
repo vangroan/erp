95c8ccaba9c7ef961a9d20be558dffa0ad61139e
Willem Victor <wimpievictor@gmail.com>
2016-04-05 20:32:26 +0200

    Create navigation

--- erp/urls.py
+++ erp/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from erp.views import dashboard
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^customer/', include('erp.customer.urls')),
+	url(r'^$', dashboard, name='dashboard'),
 ]
